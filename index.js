f4a00e0f44ba4b57ea044a2fcdf3a6ad1bc09673
redrumkumg <n.ronnapat@github.com>
2024-05-14 11:26:06 +0700

    fixed

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const dayNumber = 1;
+const dayNumber = 0;
 let dayType;
 
-switch (dayNumber) {
-    case 0:
-        dayType = "weekend";
-        break;
-    case 6:
-        dayType = "weekend";
-        break;
-    default:
-        dayType = "weekday";
-        break;
+if (dayNumber > 6 || dayNumber < 0) {
+    dayType = "invalid";
+} else {
+    switch (dayNumber) {
+        case 0:
+            dayType = "weekend";
+            break;
+        case 6:
+            dayType = "weekend";
+            break;
+        default:
+            dayType = "weekday";
+            break;
+    }
 }
 
-console.log(`It is the ${dayType}!`);
+console.log(dayType);
